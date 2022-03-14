32c7b5f8cc50949903a11711486cfb4891bd3893
DESKTOP-I2T6KB6\llsmk <llsmkr1057@gmail.com>
2022-03-14 20:19:48 +0900

    test 디렉토리의 security 패키지 삭제

--- src/main/java/com/sparta/team5finalproject/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/team5finalproject/security/WebSecurityConfig.java
@@ -150,4 +150,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+
 }
