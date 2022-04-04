9680be5f539e56e39e1ec5f3da3274fd9f6c3c19
CSW7057 <tjddnjs7057@gmail.com>
2022-04-04 22:30:40 +0900

    ì최초배포

--- src/test/java/com/sparta/team5finalproject/MainTest.java
+++ src/test/java/com/sparta/team5finalproject/MainTest.java
@@ -1,29 +1,29 @@
-package com.sparta.team5finalproject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class MainTest {
-
-    public static void main(String[] args) {
-//        // 로깅 테스트
-//        Logger logger = LoggerFactory.getLogger(MainTest.class);
+//package com.sparta.team5finalproject;
 //
-//        for (int count = 1; count <= 2; count++) {
-//            logger.trace("trace 로깅이야!!! {}", count);
-//            logger.debug("debug 로깅이야!!! {}", count);
-//            logger.info("info 로깅이야!!! {}", count);
-//            logger.warn("warn 로깅이야!!! {}", count);
-//            logger.error("error 로깅이야!!! {}", count);
-//        }
-//        // 로깅 테스트
-
-    }
-
-}
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.Set;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//public class MainTest {
+//
+//    public static void main(String[] args) {
+////        // 로깅 테스트
+////        Logger logger = LoggerFactory.getLogger(MainTest.class);
+////
+////        for (int count = 1; count <= 2; count++) {
+////            logger.trace("trace 로깅이야!!! {}", count);
+////            logger.debug("debug 로깅이야!!! {}", count);
+////            logger.info("info 로깅이야!!! {}", count);
+////            logger.warn("warn 로깅이야!!! {}", count);
+////            logger.error("error 로깅이야!!! {}", count);
+////        }
+////        // 로깅 테스트
+//
+//    }
+//
+//}
--- src/test/java/com/sparta/team5finalproject/model/CodyValidator.java
+++ src/test/java/com/sparta/team5finalproject/model/CodyValidator.java
@@ -1,29 +1,29 @@
-package com.sparta.team5finalproject.model;
-
-import com.sparta.team5finalproject.dto.CodyRequestDto;
-import com.sparta.team5finalproject.validator.URLValidator;
-import org.springframework.stereotype.Component;
-
-public class CodyValidator {
-    public static void validateCodyInput(CodyRequestDto requestDto, Long userId) {
-        if (userId == null || userId <= 0) {
-            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
-        }
-
-        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
-            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
-        }
-
-        if (!URLValidator.isValidUrl(requestDto.getImage())) {
-            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
-        }
-
-        if (!URLValidator.isValidUrl(requestDto.getLink())) {
-            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
-        }
-
-        if (requestDto.getLprice() <= 0) {
-            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
-        }
-    }
-}
+//package com.sparta.team5finalproject.model;
+//
+//import com.sparta.team5finalproject.dto.CodyRequestDto;
+//import com.sparta.team5finalproject.validator.URLValidator;
+//import org.springframework.stereotype.Component;
+//
+//public class CodyValidator {
+//    public static void validateCodyInput(CodyRequestDto requestDto, Long userId) {
+//        if (userId == null || userId <= 0) {
+//            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+//        }
+//
+//        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+//            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+//        }
+//
+//        if (!URLValidator.isValidUrl(requestDto.getImage())) {
+//            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+//        }
+//
+//        if (!URLValidator.isValidUrl(requestDto.getLink())) {
+//            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+//        }
+//
+//        if (requestDto.getLprice() <= 0) {
+//            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+//        }
+//    }
+//}
