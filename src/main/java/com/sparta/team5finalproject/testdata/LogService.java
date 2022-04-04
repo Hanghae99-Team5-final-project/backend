6f17a84929530c50db17433a96b889eba6a5b77e
CSW7057 <tjddnjs7057@gmail.com>
2022-04-05 03:40:03 +0900

    test10

--- src/main/java/com/sparta/team5finalproject/testdata/LogService.java
+++ src/main/java/com/sparta/team5finalproject/testdata/LogService.java
@@ -1,9 +1,11 @@
 package com.sparta.team5finalproject.testdata;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class LogService {
 
